28762b3251e12646df67678250c1e291fb0b04b4
sajal09 <sajalchaurasia09@gmail.com>
2020-07-14 11:48:17 +0530

    ddd

---
+++ video/src/App.js
@@ -0,0 +1,241 @@
+import React,{Component} from 'react';
+import './App.css';
+
+function onDisconnectedListener() {
+  console.log("onDisconnected");
+}
+
+
+
+
+function App(){
+
+    var session;  
+
+    var QB = require('../node_modules/quickblox/quickblox');
+
+    var CREDENTIALS = {
+      'appId': "84745",
+      'authKey': 'LdXtzcfrYbBjeAe',
+      'authSecret': 'dBGXnpyZWmqzTWf'
+    };
+
+    QB.init(CREDENTIALS.appId, CREDENTIALS.authKey, CREDENTIALS.authSecret);
+
+    var params = {login: 'sajal',password: 'quickblox' };  
+
+    var token = ""
+
+    QB.createSession(params, function(err, result) {
+      if(result)
+      {
+      console.log("session created");
+      console.log(result);
+      token = result.token
+      console.log(token)
+      }
+      else
+      {
+      console.log("error thrown");
+      console.error(err);// callback function
+      }
+    });
+
+    
+  QB.webrtc.onCallListener = function(currentsession, extension) {
+      console.log("iiiiiiiiiiitttttttts exeeeeeeeeectured")
+      console.log(currentsession)
+      console.log(extension)
+
+      var mediaParams = {
+        audio: true,
+        video: true,
+        options: {
+          muted: true,
+          mirror: true
+        },
+        elemId: "localVideo"
+      };
+      
+      currentsession.getUserMedia(mediaParams, function(err, stream) {
+        if (err) {
+          console.log("error in video call")
+          console.error(err)
+        } else {
+          console.log("video passed with lajas")
+          console.log(stream)
+        }
+      });
+
+      currentsession.accept(extension);
+  
+  };  
+
+  QB.webrtc.onUserNotAnswerListener = function(session, userId) {
+    console.log("User is not answering the phone")
+    console.log(session)
+    console.log(userId)
+    
+  };
+
+    
+  
+  const createus = () => {
+    var param = {
+      login: 'russs',
+      password: "someSecret",
+      full_name: "quickblox",
+      email: 'moron@gmail.com'
+    };
+    
+    QB.users.create(param, function(err, res) {
+      if (err) {
+        console.log("error in creation")
+        console.error(err)
+      } else {
+        console.log(res)
+      }
+    });
+  }
+
+
+  const destroyus = () => {
+
+    QB.destroySession(function(error) {
+      if(error)
+      {
+      console.log("error in destroy session");
+      console.error(error);
+      }
+    });
+
+  }
+
+  const chatconnect = () => {
+
+    var userCredentials = {
+      userId: 115593063,
+      password: 'quickblox'
+    };
+    
+    QB.chat.connect(userCredentials, function(error, contactList) {
+      if (error) {
+        console.log("error in creation of CHAT")
+        console.error(error)
+      } else {
+        console.log(contactList)
+      }
+    });
+
+  }
+
+  const getusers = () => {
+
+    var searchParams = {email: "moron@gmail.com"};
+    
+    QB.users.get(searchParams, function(error, user){
+      if(user){
+        console.log("users found");
+        console.log(user);
+      }
+      else{
+        console.log("error in finding users");
+        console.error(error);
+      }
+    
+});
+
+  }
+
+  const chatdisconnect = () => {
+
+    QB.chat.disconnect();
+
+    QB.chat.onDisconnectedListener = onDisconnectedListener();
+
+    console.log("chat Disconnected")
+
+  }
+
+  const videoconnect = () => {
+
+    var calleesIds = [115593241];
+    
+    var sessionType = QB.webrtc.CallType.VIDEO; // AUDIO is also possible
+    var additionalOptions = {};
+
+    session = QB.webrtc.createNewSession(calleesIds, sessionType, null, additionalOptions);
+
+    var mediaParams = {
+      audio: true,
+      video: true,
+      options: {
+        muted: true,
+        mirror: true
+      },
+      elemId: "localVideo"
+    };
+    
+    session.getUserMedia(mediaParams, function(err, stream) {
+      if (err) {
+        console.log("error in video call")
+        console.error(err)
+      } else {
+        console.log("video passed")
+        console.log(stream)
+      }
+    });
+
+    
+
+  }
+
+  const videochatwithlajas = () => {
+
+    var extension = {};
+    session.call(extension, function(error) {
+      if(error){
+        console.log("Error is occuring here");
+        console.error(error);
+      }
+    
+    });
+
+  }
+
+  const acceptvideochatwithlajas = () => {
+    
+    
+  }
+
+
+  
+    
+  return (
+    <div>
+    Sajal
+    <button onClick = {createus}>clickus</button>
+    <button onClick = {destroyus}>deleteus</button>
+    <button onClick = {getusers}>Getuser</button>
+    <button onClick = {chatconnect}>ChatConnect</button>
+    <button onClick = {chatdisconnect}>DisChatConnect</button>
+    <button onClick = {videoconnect}>VideoConnect</button>
+    <button onClick = {videochatwithlajas}>VideoChatwithlajas</button>
+    <button onClick = {acceptvideochatwithlajas}>AcceptVideoChatwithlajas</button>
+    <div>
+        <video id="localVideo" autoPlay playsInline></video>
+    </div>
+    </div>
+  );
+
+}
+export default App;
+
+
+
+
+
+
+
+
+
